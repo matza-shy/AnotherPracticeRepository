30a38e85f3cdd229811a8bfc5b888968ffb4b6ed
Shy Matza <shy.matza@gmail.com>
2021-01-20 10:25:38 +0200

    commit now all commits from: 1) Fix some space in comment 2) Add "Add" function 3) Add sub function

--- double.js
+++ double.js
@@ -1,13 +1,28 @@
-function (x)
+function double(x)
 {
 	double res = x * 2;
 	return res;
 }
 
+function add(x, y)
+{
+	double res = x + y;
+	return res;
+}
+
+
+function sub(x, y)
+{
+	double res = x - y;
+	return res;
+}
+
 function main()
 {
-	double num = 8; /* Very Volatile */
-	print(double(num));/* Why double?*/
+	num = 8; /* Very Volatile */
+	print(double(num));/* Why double? */
+	print(add(num, num));/* Addition ? */
+	print(sub(num, num));/* substraction ? */
 
 	return;
 }
