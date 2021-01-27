690e6b01a83df7b3a1d3384591c0bc61c10f36e2
Shy Matza <shy.matza@gmail.com>
2021-01-27 16:37:59 +0200

    Add "Add" function

--- double.js
+++ double.js
@@ -1,13 +1,21 @@
-function (x)
+function double(x)
 {
 	double res = x * 2;
 	return res;
 }
 
+function add(x, y)
+{
+	double res = x + y;
+	return res;
+}
+
+
 function main()
 {
 	double num = 8; /* Very Volatile */
 	print(double(num));/* Why double? */
+	print(add(num, num));/* Addition ? */
 
 	return;
 }
